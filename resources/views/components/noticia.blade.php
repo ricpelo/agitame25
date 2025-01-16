de86d1d10d8d58a41a6632a8238b2ef4e8ca9800
Ricardo Pérez <ricpelo@gmail.com>
2025-01-16 19:41:53 +0100

    Corrige posición de imagen en noticia

--- resources/views/components/noticia.blade.php
+++ resources/views/components/noticia.blade.php
@@ -1,11 +1,11 @@
 @props(['noticia'])
 
-<div class="inline-flex w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<div class="flex justify-between w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <div>
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-blue-500 hover:underline"><a href="{{ $noticia->url }}">{{ $noticia->titular }}</a></h5>
         <p class="font-normal text-gray-700 dark:text-gray-400">{{ $noticia->descripcion }}</p>
     </div>
-    <div class="justify-self-end">
+    <div>
         <img src="{{ $noticia->imagen }}" />
     </div>
 </div>
