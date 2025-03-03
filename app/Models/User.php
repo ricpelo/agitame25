a79edb4177270a15e9cd5b76ccbf04e436e5a2f2
Ricardo Pérez <ricpelo@gmail.com>
2025-03-03 19:54:56 +0100

    Migraciones y modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Noticia::class);
     }
+
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class);
+    }
 }
